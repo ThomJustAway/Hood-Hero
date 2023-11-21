a4edcba55b7636ce3f64b7f3571b772cbe205890
Thom Low <thomthomlow@gmail.com>
2023-11-22 00:39:14 +0800

    update
    just making some changes to the movement script

--- Hood-hero/Assets/Scripts/PlayerMovement.cs
+++ Hood-hero/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,10 @@ public class PlayerMovement : MonoBehaviour
     float horizontal;
     float vertical;
     public float speed = 5f;
-    Rigidbody2D rb;
-
+    public float rotationSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,10 +18,20 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movementVector = new Vector3(horizontal, vertical, 0);
+
+        if (movementVector != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.FromToRotation(transform.rotation.eulerAngles, movementVector);
+            transform.Rotate(rotation.eulerAngles);
+        }
+
+        transform.position += movementVector.normalized * speed * Time.deltaTime; //move without rotation affecting
+        Debug.DrawRay(transform.position, transform.up * 10 , Color.green);
     }
 
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector3(horizontal * speed, vertical * speed, speed);
-    }
+    //private void FixedUpdate()
+    //{
+    //    rb.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+    //}
 }
