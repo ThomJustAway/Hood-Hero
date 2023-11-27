c86c1deafc88e5c687f6f7fa94064edddaa710e4
Ian Wong <ianw35091@gmail.com>
2023-11-27 20:50:07 +0800

    Update
    AIGHT THOM I'LL PUSH STFU

---
+++ Hood-hero/Assets/Scripts/Problem script/Scriptable/OneServiceProblemSelectorScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using Problem;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Problem_script
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/AddProblem", order = 1)]
+    public class OneServiceProblemSelectorScriptableObject : ScriptableObject
+    {
+        public MainProblem mainProbelm;
+        public SubProblem[] subProblems;
+
+        
+    }
+}
