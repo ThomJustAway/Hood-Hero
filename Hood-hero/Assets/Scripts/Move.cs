e962be7bd3465ba095028bd33786018732fbaf96
SkyZero123 <darrylgoh0109@gmail.com>
2023-12-05 16:22:36 +0800

    Animation
    change face when using the keys to move

--- Hood-hero/Assets/Scripts/Move.cs
+++ Hood-hero/Assets/Scripts/Move.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+//using 
 
 
 public class Move : MonoBehaviour
@@ -9,7 +10,8 @@ public class Move : MonoBehaviour
     
     public EManager mEManager;
     public Rigidbody2D rb;
-    public Sprite playerSprite;
+    //public Sprite playerSprite;
+    public Animator player;
 
 
 
@@ -26,7 +28,7 @@ public class Move : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         mEManager = GetComponent<EManager>();
-        playerSprite = GetComponent<Sprite>();
+        //playerSprite = GetComponent<Sprite>();
     }
     
     // Update is called once per frame
@@ -43,26 +45,43 @@ public class Move : MonoBehaviour
     {
 
         rb.velocity = Vector2.zero;
-        
+        //playerSprite.
     }
     public void MoveUp()
     {
         rb.velocity = Vector2.up * mWalkSpeed;
+        player.SetBool("up",true);
+        player.SetBool("right", false);
+        player.SetBool("down", false);
+        player.SetBool("left", false);
+
     }
 
     public void MoveDown()
     {
         rb.velocity = Vector2.down * mWalkSpeed;
+        player.SetBool("down", true);
+        player.SetBool("up", false);
+        player.SetBool("right", false);
+        player.SetBool("left", false);
     }
 
     public void MoveLeft()
     {
         rb.velocity = Vector2.left * mWalkSpeed;
+        player.SetBool("left", true);
+        player.SetBool("up", false);
+        player.SetBool("right", false);
+        player.SetBool("down", false);
     }
 
     public void MoveRight()
     {
         rb.velocity = Vector2.right * mWalkSpeed;
+        player.SetBool("right", true);
+        player.SetBool("up", false);
+        player.SetBool("down", false);
+        player.SetBool("left", false);
     }
     
 
