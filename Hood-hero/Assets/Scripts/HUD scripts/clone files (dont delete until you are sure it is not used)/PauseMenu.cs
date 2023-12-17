c5534033630ffe211b167609965bd1766375eec8
Thom Low <thomthomlow@gmail.com>
2023-12-17 15:13:35 +0800

    finish merge branch with chris and final branch

--- Hood-hero/Assets/Scripts/HUD scripts/clone files (dont delete until you are sure it is not used)/PauseMenu.cs
+++ Hood-hero/Assets/Scripts/HUD scripts/clone files (dont delete until you are sure it is not used)/PauseMenu.cs
@@ -3,57 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
-{
-    public static bool GameIsPaused = false;
-
-    public GameObject pauseMenuUI;
-
-    void Update()
+namespace extracopy{
+    public class PauseMenu : MonoBehaviour
     {
-        // Check for Escape key input to toggle between pause and resume
-        if (Input.GetKeyUp(KeyCode.Escape))
+        public static bool GameIsPaused = false;
+
+        public GameObject pauseMenuUI;
+
+        void Update()
         {
-            if (GameIsPaused)
+            // Check for Escape key input to toggle between pause and resume
+            if (Input.GetKeyUp(KeyCode.Escape))
             {
-                Resume();
+                if (GameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
             }
-            else
-            {
-                Pause();
-            }
+        }
+
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            // Restore the time scale to normal
+            Time.timeScale = 1f;
+            // Update the pause state
+            GameIsPaused = false;
+        }
+
+        public void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            // Set the time scale to pause the game
+            Time.timeScale = 0f;
+            // Update the pause state
+            GameIsPaused = true;
+        }
+
+        public void LoadMenu(string sceneName)
+        {
+            // Restore the time scale to normal
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void QuitGame(string sceneName)
+        {
+            // Restore the time scale to normal
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(sceneName);
         }
     }
 
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        // Restore the time scale to normal
-        Time.timeScale = 1f;
-        // Update the pause state
-        GameIsPaused = false;
-    }
-
-    public void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        // Set the time scale to pause the game
-        Time.timeScale = 0f;
-        // Update the pause state
-        GameIsPaused = true;
-    }
-
-    public void LoadMenu(string sceneName)
-    {
-        // Restore the time scale to normal
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(sceneName);
-    }
-
-    public void QuitGame(string sceneName)
-    {
-        // Restore the time scale to normal
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(sceneName);
-    }
 }
--- Hood-hero/Assets/Scripts/HUD scripts/clone files (dont delete until you are sure it is not used)/SliderController.cs
+++ Hood-hero/Assets/Scripts/HUD scripts/clone files (dont delete until you are sure it is not used)/SliderController.cs
@@ -3,46 +3,107 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SliderController : MonoBehaviour
-{
-    int progress = 0;
-    public Slider slider;
-    public float fillSpeed = 1f;
+//namespace extracopy
+//{
+//    public class PauseMenu : MonoBehaviour
+//    {
+//        public static bool GameIsPaused = false;
 
-    public void UpdateProgress(int compelted, int total_Taask)  //5/7.5/10
-    {
-        if (compelted == total_Taask / 2)
-        {
-            progress = 5;
+//        public GameObject pauseMenuUI;
 
-            if (slider.value < progress)
-            {
+//        void Update()
+//        {
+//            // Check for Escape key input to toggle between pause and resume
+//            if (Input.GetKeyUp(KeyCode.Escape))
+//            {
+//                if (GameIsPaused)
+//                {
+//                    Resume();
+//                }
+//                else
+//                {
+//                    Pause();
+//                }
+//            }
+//        }
 
-                slider.value += fillSpeed * Time.deltaTime;
+//        public void Resume()
+//        {
+//            pauseMenuUI.SetActive(false);
+//            // Restore the time scale to normal
+//            Time.timeScale = 1f;
+//            // Update the pause state
+//            GameIsPaused = false;
+//        }
 
-            }
-        }
-        if (compelted == total_Taask * 0.7)
-        {
-            progress = 7;
+//        public void Pause()
+//        {
+//            pauseMenuUI.SetActive(true);
+//            // Set the time scale to pause the game
+//            Time.timeScale = 0f;
+//            // Update the pause state
+//            GameIsPaused = true;
+//        }
 
-            if (slider.value < progress)
-            {
+//        public void LoadMenu(string sceneName)
+//        {
+//            // Restore the time scale to normal
+//            Time.timeScale = 1f;
+//            SceneManager.LoadScene(sceneName);
+//        }
 
-                slider.value += fillSpeed * Time.deltaTime;
+//        public void QuitGame(string sceneName)
+//        {
+//            // Restore the time scale to normal
+//            Time.timeScale = 1f;
+//            SceneManager.LoadScene(sceneName);
+//        }
+//    }
 
-            }
-        }
-        if (compelted == total_Taask)
-        {
-            progress = 10;
 
-            if (slider.value < progress)
-            {
+//    public class SliderController : MonoBehaviour
+//    {
+//        int progress = 0;
+//        public Slider slider;
+//        public float fillSpeed = 1f;
 
-                slider.value += fillSpeed * Time.deltaTime;
+//        public void UpdateProgress(int compelted, int total_Taask)  //5/7.5/10
+//        {
+//            if (compelted == total_Taask / 2)
+//            {
+//                progress = 5;
 
-            }
-        }
-    }
-}
+//                if (slider.value < progress)
+//                {
+
+//                    slider.value += fillSpeed * Time.deltaTime;
+
+//                }
+//            }
+//            if (compelted == total_Taask * 0.7)
+//            {
+//                progress = 7;
+
+//                if (slider.value < progress)
+//                {
+
+//                    slider.value += fillSpeed * Time.deltaTime;
+
+//                }
+//            }
+//            if (compelted == total_Taask)
+//            {
+//                progress = 10;
+
+//                if (slider.value < progress)
+//                {
+
+//                    slider.value += fillSpeed * Time.deltaTime;
+
+//                }
+//            }
+//        }
+//    }
+
+//}
+
