b04e5256cc77fb107a0a5ee7e04f0df6e7b08126
Ian Wong <ianw35091@gmail.com>
2024-01-05 20:22:05 +0800

    Unfinished

--- Hood-hero/Assets/Scripts/Npc scripts/NPC.cs
+++ Hood-hero/Assets/Scripts/Npc scripts/NPC.cs
@@ -11,13 +11,15 @@ public class NPC : MonoBehaviour
     [Header("Hints")]
     [SerializeField] private ProblemSelector problem;
     [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform problemLocation;
+    [SerializeField] private guidingArrow guidingArrow;
     private GameObject arrowReference;
     //martin improve on this and consider using button instead
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DialogueManager.Instance.OpenDialogueSession(messages);
-        if(arrowReference == null)
+        if(arrowReference != null)
         {
             EventManager.instance.AddListener(TypeOfEvent.DialogEndEvent, CreateArrow);
             EventManager.instance.AddScoringListener(DeleteArrow);
@@ -26,12 +28,13 @@ public class NPC : MonoBehaviour
 
     private void CreateArrow()
     {
-        Vector3 direction = problem.transform.position - transform.position;
-        float angle = Vector3.Angle(Vector3.up, direction);
-        Vector3 arrowPos = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+        AssignArrow(problemLocation);
+        //Vector3 direction = problem.transform.position - transform.position;
+        //float angle = Vector3.Angle(Vector3.up, direction);
+        //Vector3 arrowPos = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
 
-        arrowReference = Instantiate(arrowPrefab, arrowPos, Quaternion.identity, transform);
-        arrowReference.transform.Rotate(0, 0, angle); //keep it organise and putting it in the npc
+        //arrowReference = Instantiate(arrowPrefab, arrowPos, Quaternion.identity, transform);
+        //arrowReference.transform.Rotate(0, 0, angle); //keep it organise and putting it in the npc
 
         //make sure to remove the listener to prevent memory leakage
         EventManager.instance.RemoveListener(TypeOfEvent.DialogEndEvent, CreateArrow);
@@ -45,6 +48,12 @@ public class NPC : MonoBehaviour
             arrowReference.SetActive(false); //dont show it
             EventManager.instance.RemovingScoringListener(DeleteArrow);
         }
+    }
+
+    private void AssignArrow(Transform assignedProblem)
+    {
+        //guidingArrow.Problem = assignedProblem;
+        guidingArrow.StartDatCoroutine(assignedProblem);
     }
 }
  
--- Hood-hero/Assets/Scripts/Npc scripts/guidingArrow.cs
+++ Hood-hero/Assets/Scripts/Npc scripts/guidingArrow.cs
@@ -1,8 +1,9 @@
+using System.Collections;
 using UnityEngine;
 
 public class guidingArrow : MonoBehaviour
 {
-    public Transform Problem;
+    [HideInInspector] public Transform Problem;
     public float hideDist;
     private bool arrowActive = true; // Controls guide arrow visibility
 
@@ -10,20 +11,28 @@ public class guidingArrow : MonoBehaviour
     {
         if (!arrowActive) return; // If guide arrow deactivated, does nothing
 
+        //Debug.Log(Problem.position);
+
         var dir = Problem.position - transform.position;
 
-        if (dir.magnitude < hideDist)
-        {
-            arrowActive = false; // Deactivates guide arrow after finding problem game object
-            SetChildrenActive(false);
-        }
-        else
-        {
-            SetChildrenActive(true);
+        //if (dir.magnitude < hideDist)
+        //{
+        //    arrowActive = false; // Deactivates guide arrow after finding problem game object
+        //    SetChildrenActive(false);
+        //}
+        //else
+        //{
+        //    SetChildrenActive(true);
 
-            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
+        //    var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //}
+    }
+
+    public void StartDatCoroutine(Transform Problem)
+    {
+        this.Problem = Problem;
+        StartCoroutine(TestShowArrow());
     }
 
     void SetChildrenActive(bool value)
@@ -32,5 +41,25 @@ public class guidingArrow : MonoBehaviour
         {
             child.gameObject.SetActive(value);
         }
+    }
+
+    IEnumerator TestShowArrow()
+    {
+        Vector3 dir;
+        SetChildrenActive(true);
+
+        yield return new WaitForSeconds(3);
+        float lapsedTime = 0;
+        while (lapsedTime < 3)
+        {
+            dir = Problem.position - transform.position;
+            lapsedTime += Time.deltaTime;
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            Debug.Log("I AM RUNNINGNNNNGGG");
+            yield return null;
+        }
+        SetChildrenActive(false);
     }
 }
